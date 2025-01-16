a3ef804deeb42c81ad8b7f887d1879b01e9a492f
Abrar <abrarx048@gmail.com>
2025-01-16 17:48:46 +0300

    added new SYS statement

--- ABD.java
+++ ABD.java
@@ -2,6 +2,8 @@ public class ABD {
     public static void main(String[] args){
         int x=5;
         int y=4;
+        int s=6;
+        System.out.println(s);
         System.out.println(x+y);
     }
     
